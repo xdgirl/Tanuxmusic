7280fd6afc13fd8282feb9c6e5b9dc335992a3b1
xdgirl <106166192+xdgirl@users.noreply.github.com>
2023-09-11 18:24:42 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -135,4 +135,4 @@ if PING_IMG_URL:
 if START_IMG_URL:
     if START_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", START_IMG_URL):
-            START_IMG_URL = "https://te.legra.ph/file/0c373d33859cd378dcafe.jpg"
+            START_IMG_URL = "https://telegra.ph/file/05bbd8715d29b441bc2d1.jpg"
